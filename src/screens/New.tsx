fefdaf19eeef2aea8ec20669aa9e0a531daa9ba1
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-01-26 23:27:12 -0300

    feat: created create new habit screen

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
+
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
 
@@ -25,11 +29,18 @@ export function New() {
   }
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <BackButton />
         <Text className="mt-6 text-white font-extrabold text-3xl">Criar hábito</Text>
         <Text className="mt-6 text-white font-semibold text-base">Qual seu comprometimento?</Text>
-        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          placeholder="Ex.: Exercitar, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+        />
         <Text className="font-semibold mt-4 mb-3 text-white text-base">Qual a recorrência?</Text>
         {availableWeekDays.map((weekDay, i) => (
           <CheckBox
@@ -39,6 +50,14 @@ export function New() {
             onPress={() => handleToggleWeekDay(i)}
           />
         ))}
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+          <Text className="font-semibold text-white text-base ml-2">Confirmar</Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
