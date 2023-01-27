1efbd8aa013248bb9cbce9ad78dc735718d7bfba
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-01-26 23:21:52 -0300

    feat: created week days checkboxes

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,5 +1,45 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { ScrollView, Text, TextInput, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import { CheckBox } from "../components/CheckBox";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feita",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function New() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) => prevState.filter((weekDay) => weekDay !== weekDayIndex));
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+        <Text className="mt-6 text-white font-extrabold text-3xl">Criar hábito</Text>
+        <Text className="mt-6 text-white font-semibold text-base">Qual seu comprometimento?</Text>
+        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">Qual a recorrência?</Text>
+        {availableWeekDays.map((weekDay, i) => (
+          <CheckBox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(i)}
+            onPress={() => handleToggleWeekDay(i)}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
 }
