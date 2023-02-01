e854e2f3f1368abcfda3f563d72e0af45183e721
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-02-01 16:17:03 -0300

    feat: sending habit status to api

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -11,6 +11,7 @@ import { ProgressBar } from "../components/ProgressBar";
 import { CheckBox } from "../components/CheckBox";
 import { Loading } from "../components/Loading";
 import { EmptyHabit } from "../components/EmptyHabit";
+import clsx from "clsx";
 
 interface Params {
   date: string;
@@ -33,6 +34,7 @@ export function Habit() {
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
+  const isDateInPast = parsedDate.endOf("day").isBefore(new Date());
   const dayOfWeek = parsedDate.format("dddd");
   const dayAndMonth = parsedDate.format("DD/MM");
 
@@ -56,10 +58,16 @@ export function Habit() {
   }
 
   async function handleToggleHabit(habitId: string) {
-    if (completedHabits.includes(habitId)) {
-      setCompletedHabits((prevState) => prevState.filter((habit) => habit !== habitId));
-    } else {
-      setCompletedHabits((prevState) => [...prevState, habitId]);
+    try {
+      await api.patch(`/habits/${habitId}/toggle`);
+      if (completedHabits.includes(habitId)) {
+        setCompletedHabits((prevState) => prevState.filter((habit) => habit !== habitId));
+      } else {
+        setCompletedHabits((prevState) => [...prevState, habitId]);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Ops", "Não foi possível editar o status do hábito");
     }
   }
 
@@ -81,13 +89,18 @@ export function Habit() {
         <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">{dayOfWeek}</Text>
         <Text className="text-white font-extrabold text-3xl">{dayAndMonth}</Text>
         <ProgressBar progress={habitsProgress} />
-        <View className="mt-6">
+        <View
+          className={clsx("mt-6", {
+            ["opacity-50"]: isDateInPast,
+          })}
+        >
           {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map((habit) => (
               <CheckBox
                 key={habit.id}
                 title={habit.title}
                 checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
                 onPress={() => handleToggleHabit(habit.id)}
               />
             ))
@@ -95,6 +108,11 @@ export function Habit() {
             <EmptyHabit />
           )}
         </View>
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada
+          </Text>
+        )}
       </ScrollView>
     </View>
   );
